5758c486f3d6a20a8606aa042f1eb97c26b16c7e
fatihyanik <fatihyanik07@gmail.com>
2022-10-31 21:58:27 +0100

    initial folder structure and added styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import About from './components/About/About'
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/styles/Theme";
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Projects from './components/Projects/Projects';
+import GlobalStyles from './components/styles/Global';
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <ThemeProvider theme={theme}>
+            <>
+                <GlobalStyles />
+                <Navbar />
+                <Home />
+                <Projects />
+                <About />
+                <Footer />
+            </>
+        </ThemeProvider>
+    )
 }
 
 export default App
