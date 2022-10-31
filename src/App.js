385676033f6d6aa7cf506612734ffaf13515bbde
fatihyanik <fatihyanik07@gmail.com>
2022-10-31 21:34:09 +0100

    again from stratch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import FooterNav from './Components/FooterNav/FooterNav';
-import Navbar from './Components/Navbar/Navbar';
-import Home from './Pages/Home/Home';
-import PortfolioPage from './Pages/PortfolioPage/PortfolioPage';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/portfolio' exact element={<PortfolioPage />} />
-        </Routes>
-        <FooterNav />
-      </BrowserRouter>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
